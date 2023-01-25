9a13b60d08846f5cde2617e9c31c11641600f9fa
Abass Adebayo A <greatmoh007@gmail.com>
2023-01-25 18:55:21 +0100

    Method-Question1

---
+++ JALA Academy/MethodQuestions/MethodQuestions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 1:
+            int a = 5;
+            int b = 6;
+
+            Console.WriteLine($"The sum of {a} and {b} is: " + PrintSum(a, b));
+            Console.WriteLine($"The multiplication of {a} and {b} is: " + PrintMult(a, b));
+
+        }
+        
+        public static int PrintSum(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public static int PrintMult(int x, int y)
+        {
+           return x * y;
+           
+        }
+    }
+    
+   
+}
