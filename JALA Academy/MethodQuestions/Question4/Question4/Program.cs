59d501580842244f8a317398b385e5d98661abb4
Abass Adebayo A <greatmoh007@gmail.com>
2023-01-26 15:54:49 +0100

    Question 4

---
+++ JALA Academy/MethodQuestions/Question4/Question4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayValue = {"Adebayo", "Ayandele", "Gbadebo"}; 
+            
+            Console.Write("The Array values are: ");
+            PrintArray(arrayValue);
+        }
+
+        public static string[] PrintArray(string [] arrayItems)
+        {
+            string [] arrayBucket = {};
+            string space = " ";
+            foreach (var item in arrayItems)
+            {
+                
+                Console.Write(string.Join(' ', item + space));
+                
+            }
+
+            return arrayBucket.ToArray();
+        } 
+    }
+}
