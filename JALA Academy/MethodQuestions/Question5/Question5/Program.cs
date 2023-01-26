65cf12e8e8b0628e87c51c53a347e0dc1d6817cb
Abass Adebayo A <greatmoh007@gmail.com>
2023-01-26 10:36:30 +0100

    Question 5

---
+++ JALA Academy/MethodQuestions/Question5/Question5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 5:
+            int nTerms = 5;
+            Console.Write($"The even number of {nTerms} terms are: ");
+            Console.WriteLine(String.Join(' ', PrintEvenNumberNTerms(nTerms)));
+        }
+
+        public static string PrintEvenNumberNTerms(int n)
+        {
+            string empty = string.Empty;
+            string space = " ";
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    empty += i + space;
+                }
+            }
+
+            return empty;
+        }
+    }
+}
